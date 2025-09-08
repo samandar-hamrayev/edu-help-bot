bd8712a250bfefead0a1a9ccbfde7fd271e483a0
hamrayev7 <samandarhamrayev555@gmail.com>
2025-09-08 08:15:32 +0500

    added unfold custom

--- core/admin.py
+++ core/admin.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
 from .models import TelegramUser, Question, Answer
+from unfold.admin import ModelAdmin as UnfoldModelAdmin
+from django.contrib.auth.models import User, Group
 
 @admin.register(TelegramUser)
-class TelegramUserAdmin(admin.ModelAdmin):
+class TelegramUserAdmin(UnfoldModelAdmin):
     list_display = ('telegram_id', 'username', 'is_admin')
 
 @admin.register(Question)
-class QuestionAdmin(admin.ModelAdmin):
+class QuestionAdmin(UnfoldModelAdmin):
     list_display = ('id', 'user', 'text', 'is_answered', 'created_at')
     list_filter = ('is_answered',)
 
 @admin.register(Answer)
-class AnswerAdmin(admin.ModelAdmin):
+class AnswerAdmin(UnfoldModelAdmin):
     list_display = ('question', 'responder', 'created_at')
+
+admin.site.unregister(Group)
